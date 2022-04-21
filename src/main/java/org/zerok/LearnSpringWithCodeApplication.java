e5ceda5264ef34048c50cfce9b09f71cb21324e3
Seoyeon <acc8837@naver.com>
2022-04-21 10:22:17 +0900

    init project

---
+++ src/main/java/org/zerok/LearnSpringWithCodeApplication.java
@@ -0,0 +1,13 @@
+package org.zerok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringWithCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringWithCodeApplication.class, args);
+	}
+
+}
